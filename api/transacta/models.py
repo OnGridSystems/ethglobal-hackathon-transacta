b131e1bf7857d31ff53621e1acc3744ed1b0e306
Vadim Volkov <v.volkov@ongrid.pro>
2023-03-15 17:50:10 +0400

    first commit to bootstrap the basic API

---
+++ api/transacta/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Status(models.Model):
+    chain_id = models.PositiveIntegerField(unique=True)
+    indexed_block = models.PositiveBigIntegerField()
+
+    class Meta:
+        verbose_name_plural = "statuses"
+
+
+class Token(models.Model):
+    token_id = models.PositiveIntegerField(unique=True)
+    owner = models.CharField(max_length=255, verbose_name='Owners')
+    image = models.FileField(upload_to='images/')
+    chain_id = models.PositiveIntegerField()
+    tx = models.CharField(max_length=255)
+    block_number = models.PositiveBigIntegerField()
+    skill = models.IntegerField(default=0)
+    date_updated = models.DateTimeField(auto_now=True)
+    blockchain_timestamp = models.PositiveBigIntegerField(null=True, default=None)
+    ipfs_uri_image = models.CharField(max_length=100, verbose_name='ifps uri image')
+
+    def __str__(self):
+        return f"<Token Object>: Owner {self.owner} has token with id={self.token_id} on chain with id={self.chain_id}."
