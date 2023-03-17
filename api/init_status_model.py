a5cad074adfc2c63be077a4465b5af10b420b3a1
Vadim Volkov <v.volkov@ongrid.pro>
2023-03-17 19:54:41 +0400

    minor fixes, preliminary deploy confirmed

---
+++ api/init_status_model.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+import django
+
+log = logging.getLogger(__name__)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+
+def main():
+    # bsc config
+    Status.objects.get_or_create(
+        chain_id=os.environ['BSC_CHAIN_ID'],
+        defaults={
+            'indexed_block': os.environ['BSC_START_BLOCK']
+        })
+
+
+    # eth config
+    Status.objects.get_or_create(
+        chain_id=os.environ['ETH_CHAIN_ID'],
+        defaults={
+            'indexed_block': os.environ['ETH_START_BLOCK']
+        })
+
+    # polygon config
+    Status.objects.get_or_create(
+        chain_id=os.environ['POLYGON_CHAIN_ID'],
+        defaults={
+            'indexed_block': os.environ['POLYGON_START_BLOCK']
+        })
+
+    log.info('successfully created')
+
+
+if __name__ == '__main__':
+    django.setup()
+    from transacta.models import Status
+    main()
