5d1cb15fcc9fefb297b6cac8acba1581fd733daa
njajkes <naikes.nikolait@yandex.ru>
2023-03-21 23:13:32 +0300

    chore(networks): updated networks

--- app/src/constants.js
+++ app/src/constants.js
@@ -3,7 +3,7 @@ import networks from './networks.json';
 export const supportedChainIds = Object.keys(networks).map((id) => Number(id));
 
 export const networksLogos = {
-  5: '/img/networks/mainnet.svg',
+  280: '/img/networks/zkSync.svg',
   534353: '/img/networks/Scroll.svg',
   1442: '/img/networks/polygon.svg',
 };
