6d88c51096a943a662a62138d3fc9e8e5f80cace
Vadim Volkov <v.volkov@ongrid.pro>
2023-03-22 11:59:03 +0400

    fixed model

--- api/transacta/models.py
+++ api/transacta/models.py
@@ -12,7 +12,6 @@ class Status(models.Model):
 class Token(models.Model):
     token_id = models.PositiveIntegerField(unique=True)
     owner = models.CharField(max_length=255, verbose_name='Owners')
-    image = models.FileField(upload_to='images/')
     chain_id = models.PositiveIntegerField()
     tx = models.CharField(max_length=255)
     block_number = models.PositiveBigIntegerField()
