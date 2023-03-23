471d7b60745c973754036e1a14c1385cb329d18f
njajkes <naikes.nikolait@yandex.ru>
2023-03-24 00:24:18 +0300

    feat: added abis

---
+++ app/src/api/abis/index.js
@@ -0,0 +1,13 @@
+import L2BridgeAbi from './l2bridge.json';
+import L1BridgeAbi from './l1bridge.json';
+import L2TokenAbi from './l2token.json';
+import L1TokenAbi from './l1token.json';
+
+const abis = {
+  L1BridgeAbi,
+  L1TokenAbi,
+  L2BridgeAbi,
+  L2TokenAbi
+};
+
+export default abis;
