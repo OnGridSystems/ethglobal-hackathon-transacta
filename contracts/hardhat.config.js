304fddf5548cd2d98426364aed1b52fdf30d07e9
Vadim Volkov <v.volkov@ongrid.pro>
2023-03-23 11:01:32 +0400

    extracted deploy to goerli

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,24 +1,24 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+require("dotenv").config()
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   namedAccounts: {
     deployer: {
       default: 0,
+    },
   },
   networks: {
-    hardhat: {
-      
-    },
-    goerli: { 
-      url: "https://eth-goerli.g.alchemy.com/v2/tmn4Rf7lh4ezew016QX0cDPAmAKb2KBz",
-      accounts: [process.env.PRIVATE_KEY.toString()],
-      chainId: 5
+      goerli: { 
+        url: "https://eth-goerli.g.alchemy.com/v2/tmn4Rf7lh4ezew016QX0cDPAmAKb2KBz",
+        accounts: [process.env.PRIVATE_KEY.toString()],
+        chainId: 5
+        },
       },
-    },
-    etherscan: {
-      apiKey: "PB2IP5B45PHY3ZT7CZ2MGFDEFMIFGHAZ4I"
-    }
-  }
-};
+      etherscan: {
+        apiKey: `${process.env.API_KEY.toString()}`
+      }
+    };
