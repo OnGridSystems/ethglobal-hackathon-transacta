d611e6cee0b93c3f31cf14390fd60e54e4252e16
Vadim Volkov <v.volkov@ongrid.pro>
2023-03-24 11:56:40 +0400

    added the minting script

--- contracts/deploy/mintExampleNFTs.js
+++ contracts/deploy/mintExampleNFTs.js
@@ -1,11 +1,11 @@
 module.exports = async ({deployments}) => {
     const hre = require("hardhat")
     const ethers = hre.ethers;
-    const Contract = await ethers.getContractFactory(deployments.get("ExampleNFT").address);
-    const contract = Contract.attach(deployments.get("ExampleNFT").address);
+    const address = (await deployments.get("ExampleNFT")).address;
+    const Contract = await ethers.getContractFactory("ExampleNFT");
+    const contract = Contract.attach(address);
     for(var i = 1; i <= 10; i++) {
-        contract.mint.call(process.env.DEPLOYER_WALLET);
+        console.log(await contract.mint(process.env.DEPLOYER_WALLET));
     }
 }
 module.exports.tags = ['mintExampleNFTs'];
-//module.exports.dependencies = ['ExampleNFT'];
