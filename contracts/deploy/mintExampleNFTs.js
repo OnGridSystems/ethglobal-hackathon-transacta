b6c532be5f520cb8e3ca2151b00b0f9335834b09
Vadim Volkov <v.volkov@ongrid.pro>
2023-03-24 12:25:33 +0400

    added dependencies

--- contracts/deploy/mintExampleNFTs.js
+++ contracts/deploy/mintExampleNFTs.js
@@ -4,8 +4,9 @@ module.exports = async ({deployments}) => {
     const address = (await deployments.get("ExampleNFT")).address;
     const Contract = await ethers.getContractFactory("ExampleNFT");
     const contract = Contract.attach(address);
-    for(var i = 1; i <= 10; i++) {
+    for(var i = 1; i <= 1; i++) {
         console.log(await contract.mint(process.env.DEPLOYER_WALLET));
     }
 }
 module.exports.tags = ['mintExampleNFTs'];
+module.exports.dependencies = ['ExampleNFT']
