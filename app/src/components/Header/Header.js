2ecb0a8f2f167c0cc0ebb4a082b509408f621ae3
Kirill Varlamov <kirill@ongrid.pro>
2023-03-24 10:36:18 +0400

    feat: add react router, create transactions page

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import Web3Status from '../Web3Status';
 import NetworkSelect from '../NetworkSelect';
 import { AppHeader, LogoContainer, ProjectName } from './Header.styled';
+import { Box } from '@mui/material';
+import PagesLink from '../PagesLink';
 
 const Header = ({ userAddress, connectWallet, switchNetwork, chainId }) => {
   const changeNetwork = (e) => {
@@ -12,19 +14,20 @@ const Header = ({ userAddress, connectWallet, switchNetwork, chainId }) => {
       <LogoContainer>
         <img
           alt='transacta'
-          src='img/Logo_Transacta.svg'
+          src='/img/Logo_Transacta.svg'
           style={{ width: '44px' }}
         />
         <ProjectName variant='h1' component='h2'>
           Transacta
         </ProjectName>
       </LogoContainer>
-      <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+      <PagesLink />
+      <Box display='flex' gap='20px' alignItems='center'>
         {!!chainId && (
           <NetworkSelect chainId={chainId} changeNetwork={changeNetwork} />
         )}
         <Web3Status userAddress={userAddress} connectWallet={connectWallet} />
-      </div>
+      </Box>
     </AppHeader>
   );
 };
