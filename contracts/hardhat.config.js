3536e53f1a3be681eb6ca214b0a96dbfe31dd0e4
noro1995 <n.kokhlikyan@ongrid.org>
2023-04-10 10:48:59 +0400

    chore: contract for send message from L1 to L2 ready

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,33 +1,33 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-ethers");
-require("hardhat-deploy");
-require("dotenv").config()
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-deploy');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-  paths: {
-    artifacts: "artifacts",
-    cache: "cache",
-    deploy: "deploy",
-    deployments: "deployments",
-    imports: "imports",
-    sources: "contracts",
-    tests: "test",
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    solidity: '0.8.18',
+    paths: {
+        artifacts: 'artifacts',
+        cache: 'cache',
+        deploy: 'deploy',
+        deployments: 'deployments',
+        imports: 'imports',
+        sources: 'contracts',
+        tests: 'test',
     },
-  },
-  networks: {
-      goerli: { 
-        url: "https://eth-goerli.g.alchemy.com/v2/tmn4Rf7lh4ezew016QX0cDPAmAKb2KBz",
-        accounts: [process.env.PRIVATE_KEY.toString()],
-        chainId: 5
+    namedAccounts: {
+        deployer: {
+            default: 0,
         },
-      },
-      etherscan: {
-        apiKey: `${process.env.API_KEY}`
-      }
-    };
+    },
+    networks: {
+        goerli: {
+            url: 'https://eth-goerli.g.alchemy.com/v2/tmn4Rf7lh4ezew016QX0cDPAmAKb2KBz',
+            accounts: [process.env.PRIVATE_KEY.toString()],
+            chainId: 5,
+        },
+    },
+    etherscan: {
+        apiKey: `${process.env.API_KEY}`,
+    },
+};
--- contracts/zkSync/hardhat.config.ts
+++ contracts/zkSync/hardhat.config.ts
@@ -1,26 +1,50 @@
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
-
-import {config} from "dotenv";
+import '@matterlabs/hardhat-zksync-deploy';
+import '@matterlabs/hardhat-zksync-solc';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-etherscan';
+import '@matterlabs/hardhat-zksync-verify';
+import 'hardhat-deploy'
+import { config } from 'dotenv';
 config();
 
 module.exports = {
-  zksolc: {
-    version: "1.3.5",
-    compilerSource: "binary",
-    settings: {},
-  },
-  defaultNetwork: "zkSyncTestnet",
-
-  networks: {
-    zkSyncTestnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: process.env.ETH_URL, // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-      zksync: true,
-      accounts: [process.env.PRIVATE_KEY]
+    zksolc: {
+        version: '1.3.8',
+        compilerSource: 'binary',
+        settings: {},
     },
-  },
-  solidity: {
-    version: "0.8.17",
-  },
+    defaultNetwork: 'zkSyncTestnet',
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    networks: {
+        zkSyncTestnet: {
+            url: 'https://zksync2-testnet.zksync.dev',
+            ethNetwork: process.env.ETH_URL, // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+            zksync: true,
+            accounts: [process.env.PRIVATE_KEY],
+            verifyURL: 'https://goerli.explorer.zksync.io/contracts/verify',
+        },
+        goerli: {
+            url: process.env.ETH_URL,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 5,
+        },
+        
+    },
+    etherscan: {
+        apiKey: `${process.env.API_KEY}`
+    },
+    solidity: {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+    },
 };
